c72c161edbc119915107657c48cd7515e54b9790
Dhruv Bhanushali <dhruv_b@live.com>
2021-05-06 22:50:18 +0530

    Add Solarized light and dark theme support

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,6 @@
+import '@/styles/solarized.css'
+import './css/preview.css'
+
 export const parameters = {
   actions: {
     argTypesRegex: '^on[A-Z].*',
@@ -7,5 +10,20 @@ export const parameters = {
       color: /(background|color)$/i,
       date: /Date$/,
     },
+  },
+  themes: {
+    default: 'Dark',
+    list: [
+      {
+        name: 'Dark',
+        class: ['solarized', 'dark'],
+        color: '#002b36',
+      },
+      {
+        name: 'Light',
+        class: ['solarized', 'light'],
+        color: '#fdf6e3',
+      },
+    ],
   },
 }
