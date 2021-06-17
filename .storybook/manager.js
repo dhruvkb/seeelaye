b85368d14c9e3ea8ccceb4d6fdd1f6cd54033b9f
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-18 03:12:08 +0530

    Configure static file directory

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,13 +1,11 @@
 import { addons } from '@storybook/addons'
 import { create } from '@storybook/theming'
 
-import logo from '../src/assets/seeelaye.svg'
-
 const theme = create({
   base: 'light',
   brandTitle: 'Seeelaye',
   brandUrl: 'https://github.com/dhruvkb/seeelaye',
-  brandImage: logo,
+  brandImage: 'seeelaye.svg', // copied using -s flag
 })
 
 addons.setConfig({
