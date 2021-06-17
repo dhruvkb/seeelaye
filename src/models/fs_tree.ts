e70c7ccb45fcbce0080aa333669dfd252c6bcfa0
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-18 02:50:19 +0530

    Write the class definition of a filesystem node

---
+++ src/models/fs_tree.ts
@@ -0,0 +1,103 @@
+export enum FsNodeType {
+  FILE = 'file',
+  FOLDER = 'folder'
+}
+
+export const nameExtensionSplit = (fullName: string): [string, string | null] => {
+  if (!fullName.includes('.')) {
+    return [fullName, null]
+  }
+  const lastDot = fullName.lastIndexOf('.')
+  const name = fullName.substring(0, lastDot)
+  const extension = fullName.substring(lastDot + 1)
+  return [name, extension]
+}
+
+export interface FsNodeInterface {
+  type: FsNodeType
+  name: string
+  aliases: string[]
+  children: FsNodeInterface[]
+}
+
+export class FsNode implements FsNodeInterface {
+  type: FsNodeType
+  name: string
+  aliases: string[]
+  parent!: FsNode
+  children: FsNode[]
+
+  constructor(type: FsNodeType, name: string, aliases: string[] = []) {
+    this.type = type
+    this.name = name
+    this.aliases = aliases
+
+    this.children = []
+  }
+
+  /**
+   * Determine whether this node represents a file system folder.
+   * @return {boolean} whether this node represents a file system folder
+   */
+  get isFolder(): boolean {
+    return this.isType(FsNodeType.FOLDER)
+  }
+
+  /**
+   * Determine whether this node represents a file system file.
+   * @return {boolean} whether this node represents a file system file
+   */
+  get isFile(): boolean {
+    return this.isType(FsNodeType.FILE)
+  }
+
+  /**
+   * Determine whether this node is the root of the file system.
+   * @return {boolean} whether this node is the root of the file system
+   */
+  get isRoot(): boolean {
+    return this.parent === this
+  }
+
+  /**
+   * Get all forms of the name for this node. This includes the primary name
+   * and all aliases, with and without the extension.
+   *
+   * In addition to the name and all aliases, for
+   * - files, the name without the extension
+   * - folders, the name with the trailing slash
+   * - the root node, a single slash
+   * is also accepted.
+   *
+   * Duplicates names are removed.
+   *
+   * @returns {string[]} the list of all valid names for the node
+   */
+  get allNames(): string[] {
+    const allNames = [this.name, ...this.aliases]
+    if (this.isFile) {
+      allNames.forEach((name) => {
+        const [nameMinusExtension] = nameExtensionSplit(name)
+        allNames.push(nameMinusExtension)
+      })
+    }
+    if (this.isFolder) {
+      allNames.forEach((name) => {
+        allNames.push(`${name}/`)
+      })
+    }
+    if (this.isRoot) {
+      allNames.push('/')
+    }
+    return Array.from(new Set(allNames))
+  }
+
+  /**
+   * Determine whether this node represents a file system file or folder.
+   * @param {FsNodeType} type - the type of the node to compare this node with
+   * @return {boolean} whether this node is of the given node type
+   */
+  isType(type: FsNodeType): boolean {
+    return this.type === type
+  }
+}
---
+++ tests/unit/models/fs_tree.spec.ts
@@ -0,0 +1,99 @@
+import { FsNode, FsNodeType, nameExtensionSplit } from '@/models/fs_tree'
+
+describe('nameExtensionSplit', () => {
+  it('handles node names without extension', () => {
+    const [name, ext] = nameExtensionSplit('name')
+    expect(name).toEqual('name')
+    expect(ext).toBeNull()
+  })
+
+  it('handles simple names', () => {
+    const [name, ext] = nameExtensionSplit('name.ext')
+    expect(name).toEqual('name')
+    expect(ext).toEqual('ext')
+  })
+
+  it('handles names with multiple dots', () => {
+    const [name, ext] = nameExtensionSplit('name.name.ext')
+    expect(name).toEqual('name.name')
+    expect(ext).toEqual('ext')
+  })
+
+  it('handles names ending with a dot', () => {
+    const [name, ext] = nameExtensionSplit('name.')
+    expect(name).toEqual('name')
+    expect(ext).toEqual('')
+  })
+
+  it('handles names starting with a dot', () => {
+    const [name, ext] = nameExtensionSplit('.ext')
+    expect(name).toEqual('')
+    expect(ext).toEqual('ext')
+  })
+})
+
+describe('FsNode', () => {
+  describe('constructor', () => {
+    it('requires a type and a name to construct the object', () => {
+      const node = new FsNode(FsNodeType.FILE, 'name')
+      expect(node).toBeInstanceOf(FsNode)
+    })
+  })
+
+  describe('isFile/isFolder', () => {
+    it('returns true for files', () => {
+      const node = new FsNode(FsNodeType.FILE, 'name')
+      expect(node.isFile).toBe(true)
+      expect(node.isFolder).toBe(false)
+    })
+
+    it('returns false for folders', () => {
+      const node = new FsNode(FsNodeType.FOLDER, 'name')
+      expect(node.isFile).toBe(false)
+      expect(node.isFolder).toBe(true)
+    })
+  })
+
+  describe('isRoot', () => {
+    it('checks if node is parent of itself', () => {
+      const node = new FsNode(FsNodeType.FOLDER, '~')
+      expect(node.isRoot).toEqual(false)
+      node.parent = node
+      expect(node.isRoot).toEqual(true)
+    })
+  })
+
+  describe('allNames', () => {
+    it('includes all names and aliases', () => {
+      const node = new FsNode(FsNodeType.FOLDER, 'name', ['alias'])
+      expect(node.allNames).toContain('name')
+      expect(node.allNames).toContain('alias')
+    })
+
+    it('adds trailing slash for folders', () => {
+      const node = new FsNode(FsNodeType.FOLDER, 'name', ['alias'])
+      expect(node.allNames).toContain('name/')
+      expect(node.allNames).toContain('alias/')
+    })
+
+    it('includes just a slash for root directory', () => {
+      const node = new FsNode(FsNodeType.FOLDER, '~')
+      node.parent = node
+      expect(node.allNames).toContain('/')
+    })
+  })
+
+  describe('isType', () => {
+    it('checks type for files', () => {
+      const node = new FsNode(FsNodeType.FILE, 'name')
+      expect(node.isType(FsNodeType.FILE)).toEqual(true)
+      expect(node.isType(FsNodeType.FOLDER)).toEqual(false)
+    })
+
+    it('checks type for folders', () => {
+      const node = new FsNode(FsNodeType.FOLDER, 'name')
+      expect(node.isType(FsNodeType.FILE)).toEqual(false)
+      expect(node.isType(FsNodeType.FOLDER)).toEqual(true)
+    })
+  })
+})
