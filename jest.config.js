8874da1540301dfde27e0cd0e32b452eb8efb777
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-22 12:30:41 +0530

    Remove transform that is no longer needed

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,3 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  transform: {
-    '^.+\\.vue$': 'vue-jest', // Not needed after upgrade to vue-jest@5
-  },
 }
