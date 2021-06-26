d6e6104cda8c24df2e50326bdcd0a7939dd03a99
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-27 01:45:54 +0530

    Export more classes items through the index

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,13 @@
 export { Seeelaye } from '@/base/seeelaye'
 export { seeelayeKey, useSeeelaye } from '@/base/injection'
 
+// Models
+export { nameExtSplit, FsNodeType, FsNode } from '@/models/fs_tree'
+export { Interaction } from '@/models/interaction'
+
+// Vuex
+export { getVuexModule } from '@/store'
+export { TerminalState } from '@/store/state'
+
 // eslint-disable-next-line import/export
 export * from '@/global_extensions'
