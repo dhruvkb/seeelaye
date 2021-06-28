02adcc9d6508a0620f882f09b2387fdc69760a63
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-28 11:08:38 +0530

    Extract binary options into a separate exported interface

--- src/global_extensions.ts
+++ src/global_extensions.ts
@@ -2,17 +2,21 @@ import type { IArg } from '@/models/arg'
 
 import { Seeelaye } from '@/base/seeelaye'
 
+export interface BinaryOptions {
+  command?: string
+  description?: string
+  argSpec?: {
+    posArgs?: IArg[]
+    kwArgs?: IArg[]
+  }
+}
+
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
     $seeelaye: Seeelaye
   }
 
-  export interface ComponentCustomOptions {
-    command?: string
-    description?: string
-    argSpec?: {
-      posArgs?: IArg[]
-      kwArgs?: IArg[]
-    }
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface ComponentCustomOptions extends BinaryOptions {
   }
 }
