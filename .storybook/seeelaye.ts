d0a606cb4715b1e1cb2e0e6315b183b3592b44c9
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-01 17:24:35 +0530

    Ensure that tree generation happens only once

--- .storybook/seeelaye.ts
+++ .storybook/seeelaye.ts
@@ -1,10 +1,17 @@
 import { Seeelaye } from '../src'
 import store from './store'
 
+import { getTree } from '../tests/fixtures/fs_tree.fix'
+
 const seeelaye = new Seeelaye(
   store,
   'terminal',
   {},
 )
+
+const [root, a] = getTree()
+
+seeelaye.commit('setTree', { tree: root })
+seeelaye.commit('setCurrentNode', { currentNode: a })
 
 export default seeelaye
--- .storybook/terminal_content.ts
+++ .storybook/terminal_content.ts
@@ -1,26 +1,6 @@
-import { Component } from 'vue'
-
-import { FsNode, FsNodeType, useSeeelaye } from '../src'
+import type { Component } from 'vue'
 
 export const terminalContent = (): Component => ({
-  setup() {
-    const seeelaye = useSeeelaye()
-
-    const root = new FsNode(FsNodeType.FOLDER, '~') // root/
-    const a = new FsNode(FsNodeType.FOLDER, 'a')    // ├── a/
-    const ab = new FsNode(FsNodeType.FILE, 'ab')    // │   └── ab
-    const c = new FsNode(FsNodeType.FOLDER, 'c')    // └── c/
-
-    a.parent = root
-    root.children.push(a)
-    ab.parent = a
-    a.children.push(ab)
-    c.parent = root
-    root.children.push(c)
-
-    seeelaye.commit('setTree', { tree: root })
-    seeelaye.commit('setCurrentNode', { currentNode: a })
-  },
   template: `
     <div class="terminal-content">
       <story/>
