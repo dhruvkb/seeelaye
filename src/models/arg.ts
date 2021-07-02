bc56544052dbf73da53843fefe234bebe5fac111
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-02 21:25:53 +0530

    Add `isRequired` field to argument

--- src/models/arg.ts
+++ src/models/arg.ts
@@ -4,8 +4,9 @@ import type { FsNodeType } from '@/models/fs_tree'
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface IArg<T = any> {
   name: string
-  aliases?: string[]
   description: string
+  aliases?: string[]
+  isRequired?: boolean
 
   type: Handler<T>
   default?: T
