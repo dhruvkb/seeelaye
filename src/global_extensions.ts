fcbcaf6a360d022e812b640820bf30a152f373a4
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-02 21:21:21 +0530

    Reduce global extensions to keep binary options outside of components

--- src/global_extensions.ts
+++ src/global_extensions.ts
@@ -1,24 +1,7 @@
-import type { IArg } from '@/models/arg'
-import type { FsNode } from '@/models/fs_tree'
-
 import { Seeelaye } from '@/base/seeelaye'
-
-export interface BinaryOptions {
-  command?: string
-  description?: string
-  argSpec?: {
-    posArgs?: IArg[]
-    kwArgs?: IArg[]
-  }
-}
 
 declare module '@vue/runtime-core' {
   export interface ComponentCustomProperties {
     $seeelaye: Seeelaye
-    node?: FsNode | null
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface ComponentCustomOptions extends BinaryOptions {
   }
 }
