4c7e2769cafc525084bab7398b5f840b66c32711
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-15 22:25:18 +0530

    Remove redundant interface and add missing docstrings

--- src/models/interaction.ts
+++ src/models/interaction.ts
@@ -2,53 +2,73 @@ import type { Binary } from '@/models/bin'
 
 import { FsNode } from '@/models/fs_tree'
 
-export interface IInteraction {
-  context: {
-    wd: FsNode
-  }
-
-  rawInput: string
-  input: {
-    bin: string
-    argv: string[]
-  }
-
-  output?: {
-    component: string,
-    argv: string[]
-  }
-}
-
 /**
  * A class that implements one interaction of the CLI. Each interaction consists
  * of the context, an input and the output generated based on the input and the
  * context.
  */
-export class Interaction implements IInteraction {
+export class Interaction {
   static allBins: Record<string, Binary<unknown[], unknown[]>> = {}
 
+  /**
+   * the circumstances in which command was executed; This should contain a
+   * snapshot of the terminal store, but for now it only records the working
+   * directory.
+   */
   context: {
+    /**
+     * the working directory at the time of execution of the command
+     */
     wd: FsNode
   }
 
+  /**
+   * the exact string of the input command
+   */
   rawInput: string
+  /**
+   * the processed input command; The command is split at whitespaces, with the
+   * first part being the binary and all subsequent parts being its arguments.
+   */
   input: {
+    /**
+     * the name of the binary invoked in the command
+     */
     bin: string
+    /**
+     * the list of arguments to be passed to the binary
+     */
     argv: string[]
   }
 
-  output?: {
-    component: string,
+  /**
+   * the processed output; This maps the bin from `input` to a component name
+   * and can make changes to the argument vector, if needed.
+   */
+  output!: {
+    /**
+     * the name of the component to render for the given command; Blank inputs
+     * map to `Nop` and invalid binary names map to `Bad`.
+     */
+    component: string
+    /**
+     * the list of arguments to be passed to the component in the `argv` prop
+     */
     argv: string[]
   }
 
+  /**
+   * whether this interaction should be visible in the past section of the
+   * terminal; This is `true` by default and the `clear` command sets this to
+   * `false`.
+   */
   isVisible: boolean
 
   /**
    * Create a new object of class `Interaction`.
    *
    * @param context - the circumstances in which command was executed
-   * @param rawInput - the exact string of the executed command
+   * @param rawInput - the exact string of the input command
    */
   constructor(context: { wd: FsNode }, rawInput: string) {
     this.context = context
