9945df75ccf2d9a8a4b7cfc9c5be2e374ed185e8
Dhruv Bhanushali <dhruv_b@live.com>
2021-07-20 22:03:23 +0530

    Move terminal styling into the component

--- src/containers/terminal/Terminal.vue
+++ src/containers/terminal/Terminal.vue
@@ -58,4 +58,51 @@
   })
 </script>
 
-<style scoped lang="css" src="../../styles/terminal.css"/>
+<style scoped lang="css">
+  .terminal {
+    --default-font-family: "JetBrains Mono", monospace;
+    --default-font-size: 14px;
+    --default-line-height: 1.4;
+
+    --default-height: 100%;
+
+    --default-padding: 2ex 1ch;
+
+    background: var(--color-normal-bg);
+    color: var(--color-normal-fg);
+
+    font-family: var(--font-family, var(--default-font-family));
+    font-size: var(--font-size, var(--default-font-size));
+    line-height: var(--line-height, var(--default-line-height));
+
+    height: var(--height, var(--default-height));
+
+    padding: var(--padding, var(--default-padding));
+  }
+
+  ::v-deep(.terminal-content) {
+    --default-content-max-width: 80ch;
+    max-width: var(--content-max-width, var(--default-content-max-width));
+
+    border-right: 1px solid var(--color-highlight-bg);
+  }
+
+  ::v-deep(.terminal-content) p {
+    margin: 0;
+  }
+
+  ::v-deep(.terminal-content) ul {
+    list-style: none;
+
+    padding-left: 0;
+    margin: 0;
+  }
+
+  ::v-deep(.terminal-content) a {
+    color: inherit;
+  }
+
+  ::v-deep(.terminal-content) .error {
+    color: var(--color-normal-red);
+  }
+</style>
