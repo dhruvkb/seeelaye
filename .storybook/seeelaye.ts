cbcd62f31fbcbf730866e107f98cd08ad617423d
Dhruv Bhanushali <dhruv_b@live.com>
2021-08-09 21:02:21 +0530

    Update usage of FS tree fixture

--- .storybook/seeelaye.ts
+++ .storybook/seeelaye.ts
@@ -1,11 +1,9 @@
 import { Seeelaye } from '../src'
+import { root, a } from '../tests/fixtures/fs_tree.fix'
+
 import store from './store'
 
-import { getTree } from '../tests/fixtures/fs_tree.fix'
-
 const seeelaye = new Seeelaye(store, 'terminal')
-
-const [root, a] = getTree()
 
 seeelaye.commit('setTree', { tree: root })
 seeelaye.commit('setCurrentNode', { currentNode: a })
