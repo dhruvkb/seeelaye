b1871ef666d1a52cf78246fd9f621565b69cfe29
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-03 19:32:30 +0400

    Update everything to new versions and update the build pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,28 @@
 /* eslint-disable import/no-extraneous-dependencies */
 
-import path from 'path'
+import { resolve } from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
-import ttypescript from 'ttypescript'
-import typescript from 'rollup-plugin-typescript2'
 
 /**
  * @see {@link https://vitejs.dev/config/ | Config reference}
  */
 export default defineConfig({
-  plugins: [
-    vue(),
-    { /**
-       * For exporting type declarations of Vue components
-       * @see {@link https://github.com/vitejs/vite/issues/3461#issuecomment-845240523 | Workaround }
-       */
-      ...typescript({
-        typescript: ttypescript,
-        check: false,
-      }),
-      enforce: 'pre',
-      apply: 'build',
-    },
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
       // Also see:
       // - `paths` in `tsconfig.json`
       // - `viteFinal` in `.storybook/main.ts`
-      '@': new URL('./src/', import.meta.url).pathname,
-      tests: new URL('./tests/', import.meta.url).pathname,
+      '@': resolve(__dirname, './src'),
+      tests: resolve(__dirname, './tests'),
     },
   },
   build: {
     minify: false,
     lib: {
-      entry: path.resolve(__dirname, 'src/index.ts'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'see·el·aye',
       fileName: (format) => `seeelaye.${format}.js`,
     },
